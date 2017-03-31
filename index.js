21d99da122f8beca9ef9139aeda50a93c21ae740
jimmy <jimmyjconnelly@gmail.com>
2017-03-31 14:01:24 +0100

    script test

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
 app.get('/', (req, res) => {
-  res.send('HEY THERE!')
+  res.send('HEY THERE LETS TEST DEPLOY SCRIPT!')
 })
 app.listen(3000, () => console.log('Server running on port 3000'))
